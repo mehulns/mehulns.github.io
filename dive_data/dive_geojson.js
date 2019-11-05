67721d5f75589bf572edb7a7c9fc1c7e9c2e51c8
メフル <mehulnswork@gmail.com>
2019-11-05 11:02:44 +0900

    Nov 5, Created a GeoJson JS file

---
+++ dive_data/dive_geojson.js
@@ -0,0 +1,28 @@
+var map_origin_lat = 35.15633666666667;
+var map_origin_lon = 139.60712833333332;
+
+var dive_start_lat = 35.15609666666667;
+var dive_start_lon = 139.607065;
+
+var dive_end_lat = 35.156378333333336;
+var dive_end_lon = 139.60693333333333;
+
+var dive_data = {
+	"type": "FeatureCollection",
+	"features": [
+		{ "type": "Feature", "properties": {"depth": 0.238, "color": "#ff6800"}, "geometry": { "type": "Point", "coordinates": [ 139.607065, 35.15609666666667 ]}},
+		{ "type": "Feature", "properties": {"depth": 0.219, "color": "#ff6800"}, "geometry": { "type": "Point", "coordinates": [ 139.60706666666667, 35.156095 ]}},
+		{ "type": "Feature", "properties": {"depth": 0.2141, "color": "#ff6800"}, "geometry": { "type": "Point", "coordinates": [ 139.60706833333333, 35.15609333333333 ]}},
+		{ "type": "Feature", "properties": {"depth": 0.252, "color": "#ff6800"}, "geometry": { "type": "Point", "coordinates": [ 139.60707, 35.15609166666667 ]}},
+		{ "type": "Feature", "properties": {"depth": 0.3168, "color": "#ffc600"}, "geometry": { "type": "Point", "coordinates": [ 139.60707166666666, 35.15609 ]}},
+		{ "type": "Feature", "properties": {"depth": 0.4544, "color": "#ceff29"}, "geometry": { "type": "Point", "coordinates": [ 139.607075, 35.15608833333334 ]}},
+		{ "type": "Feature", "properties": {"depth": 0.3318, "color": "#ffc600"}, "geometry": { "type": "Point", "coordinates": [ 139.60707833333333, 35.15608666666667 ]}},
+		{ "type": "Feature", "properties": {"depth": 0.242, "color": "#ff6800"}, "geometry": { "type": "Point", "coordinates": [ 139.60708166666666, 35.156085 ]}},
+		{ "type": "Feature", "properties": {"depth": 0.2669, "color": "#ff6800"}, "geometry": { "type": "Point", "coordinates": [ 139.607085, 35.156083333333335 ]}},
+		{ "type": "Feature", "properties": {"depth": 0.0954, "color": "#800000"}, "geometry": { "type": "Point", "coordinates": [ 139.60699833333334, 35.15638833333333 ]}},
+		{ "type": "Feature", "properties": {"depth": 0.0904, "color": "#800000"}, "geometry": { "type": "Point", "coordinates": [ 139.60697166666668, 35.156391666666664 ]}},
+		{ "type": "Feature", "properties": {"depth": 0.0804, "color": "#800000"}, "geometry": { "type": "Point", "coordinates": [ 139.60695666666666, 35.15638833333333 ]}},
+		{ "type": "Feature", "properties": {"depth": 0.0884, "color": "#800000"}, "geometry": { "type": "Point", "coordinates": [ 139.60695, 35.15638166666667 ]}},
+		{ "type": "Feature", "properties": {"depth": 0.0914, "color": "#800000"}, "geometry": { "type": "Point", "coordinates": [ 139.60693333333333, 35.156378333333336 ]}}
+	]
+}
