22909b78215779550aeafca707ff44ecf0cc5fa5
メフル <mehulnswork@gmail.com>
2019-11-07 08:42:14 +0900

    Update dive_geojson.js

--- 163350_163453/dive_geojson.js
+++ 163350_163453/dive_geojson.js
@@ -6,3 +6,7 @@ var dive_start_lon = 139.607085;
 
 var dive_end_lat = 35.15638833333333;
 var dive_end_lon = 139.60699833333334;
+
+var dive_start_str = '2019-10-05<br>16:33:50';
+var dive_end_str = '2019-10-05<br>16:34:53';
+
