84340a8fc29435dd9e739f4b62ee51a4821535ac
メフル <mehulnswork@gmail.com>
2019-11-06 10:32:19 +0900

    new Data

--- 163350_163453/dive_geojson.js
+++
@@ -1,8 +0,0 @@
-var map_origin_lat = 35.156083333333335;
-var map_origin_lon = 139.607085;
-
-var dive_start_lat = 35.156083333333335;
-var dive_start_lon = 139.607085;
-
-var dive_end_lat = 35.15638833333333;
-var dive_end_lon = 139.60699833333334;
