642258fcb66007396c68ba5e779b85cb261c42d2
メフル <mehulnswork@gmail.com>
2019-11-09 08:32:47 +0900

    Nov 9, Uploading new data set

---
+++ 162658_162813/dive_geojson.js
@@ -0,0 +1,12 @@
+var map_origin_lat = 35.15593833333333;
+var map_origin_lon = 139.607085;
+
+var dive_start_lat = 35.15593833333333;
+var dive_start_lon = 139.607085;
+
+var dive_end_lat = 35.156125;
+var dive_end_lon = 139.60705833333333;
+
+var dive_start_str = '2019-10-05<br>16:26:58';
+var dive_end_str = '2019-10-05<br>16:28:13';
+
