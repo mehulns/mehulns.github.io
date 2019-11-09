20e6e9735b00d846032f28c02360219c437c26f3
メフル <mehulnswork@gmail.com>
2019-11-09 11:36:47 +0900

    Create dive_geojson.js

---
+++ 163350_163453/dive_geojson.js
@@ -0,0 +1,17 @@
+var map_origin_lat = 35.156083333333335;
+var map_origin_lon = 139.607085;
+
+var dive_start_lat = 35.156083333333335;
+var dive_start_lon = 139.607085;
+
+var dive_end_lat = 35.15638833333333;
+var dive_end_lon = 139.60699833333334;
+
+var dive_name  = '<hd>Name:</hd>Misaki<br>';
+var dive_date  = '<hd>Date/Time:</hd>Oct 05, 2019 16:33:50 - 16:34:53<br>';
+var dive_dist  = '<hd>Distance:</hd>34.82 m<br>';
+var dive_depth = '<hd>Depth:</hd>0.53 m<br>';
+
+var dive_start_str = '2019-10-05<br>16:33:50';
+var dive_end_str = '2019-10-05<br>16:34:53';
+
