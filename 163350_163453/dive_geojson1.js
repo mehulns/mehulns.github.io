b01bdd36c40d32cecf10aa53c6464b016a5e733c
メフル <mehulnswork@gmail.com>
2019-11-09 10:05:20 +0900

    Create dive_geojson1.js

---
+++ 163350_163453/dive_geojson1.js
@@ -0,0 +1,18 @@
+var map_origin_lat = 35.156083333333335;
+var map_origin_lon = 139.607085;
+
+var dive_start_lat = 35.156083333333335;
+var dive_start_lon = 139.607085;
+
+var dive_end_lat = 35.15638833333333;
+var dive_end_lon = 139.60699833333334;
+
+var dive_name  = '<hd>Name:</hd> Mugachan <br>';       
+var dive_date  = '<hd>Date:</hd> 2019-10-05<br>';
+var dive_time  = '<hd>Time:</hd> 16:26:05-16:27:30<br>';
+var dive_depth = '<hd>Depth:</hd> 1.0m<br>';
+
+var dive_start_str = '2019-10-05<br>16:33:50';
+var dive_end_str = '2019-10-05<br>16:34:53';
+
+
