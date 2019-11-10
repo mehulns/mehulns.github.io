2198c9c4ce30cfd27d332afd4479c2cf72f03dba
メフル <mehulnswork@gmail.com>
2019-11-10 19:11:09 +0900

    Uploading dataset for 162658_162813

--- 162658_162813/dive_geojson.js
+++ 162658_162813/dive_geojson.js
@@ -1,12 +1,18 @@
-var map_origin_lat = 35.15593833333333;
+var map_origin_lat = 35.156083333333335;
 var map_origin_lon = 139.607085;
 
-var dive_start_lat = 35.15593833333333;
+var dive_start_lat = 35.156083333333335;
 var dive_start_lon = 139.607085;
 
-var dive_end_lat = 35.156125;
-var dive_end_lon = 139.60705833333333;
+var dive_end_lat = 35.15638833333333;
+var dive_end_lon = 139.60699833333334;
 
-var dive_start_str = '2019-10-05<br>16:26:58';
-var dive_end_str = '2019-10-05<br>16:28:13';
+var dive_name  = '<hd>Name: </hd>Misaki<br>';
+var dive_date  = '<hd>Date: </hd>October 05, 2019<br>';
+var dive_time  = '<hd>Time: </hd>16:33:50 - 16:34:53 m<br>';
+var dive_dist  = '<hd>Distance: </hd>34.82 m<br>';
+var dive_depth = '<hd>Depth: </hd>0.53 m<br>';
+
+var dive_start_str = '2019-10-05<br>16:33:50';
+var dive_end_str = '2019-10-05<br>16:34:53';
 
