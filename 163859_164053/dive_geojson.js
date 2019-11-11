a435cd452e1d98a73f401c5a6c37bc23444d91b9
メフル <mehulnswork@gmail.com>
2019-11-12 07:28:04 +0900

    Transect 163859_164053
    - New transect data

---
+++ 163859_164053/dive_geojson.js
@@ -0,0 +1,18 @@
+var map_origin_lat = 35.15633;
+var map_origin_lon = 139.60690166666666;
+
+var dive_start_lat = 35.15633;
+var dive_start_lon = 139.60690166666666;
+
+var dive_end_lat = 35.15618166666667;
+var dive_end_lon = 139.60723333333334;
+
+var dive_name  = '<hd>Name: </hd>Misaki<br>';
+var dive_date  = '<hd>Date: </hd>October 05, 2019<br>';
+var dive_time  = '<hd>Time: </hd>16:38:59 - 16:40:53 m<br>';
+var dive_dist  = '<hd>Distance: </hd>34.37 m<br>';
+var dive_depth = '<hd>Depth: </hd>0.57 m<br>';
+
+var dive_start_str = '2019-10-05<br>16:38:59';
+var dive_end_str = '2019-10-05<br>16:40:53';
+
